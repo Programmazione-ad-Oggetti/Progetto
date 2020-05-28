e08f3b3625883e733d89d5596cb807e2b4e7336b
CarmenAndreozzi <64028178+CarmenAndreozzi@users.noreply.github.com>
2020-05-28 16:24:10 +0200

    Creazione della classe madre per le statistiche

---
+++ EsameProgrammazione/src/main/java/esame/EsameProgrammazione/statistic/Statistics.java
@@ -0,0 +1,24 @@
+package esame.EsameProgrammazione.statistic;
+
+import java.util.ArrayList;
+import esame.EsameProgrammazione.model.Tweet;
+
+/////***************************************/////
+///// CREA UNA LISTA DI TWEET CHE VERRANNO  /////
+///// PASSATI AI CALCOLATORI DI STATISTICHE /////
+/////***************************************/////
+
+/** @author Antonio Colucci
+ *  @author Carmen Andreozzi
+ */
+
+public class Statistics {
+	
+	protected ArrayList<Tweet> tweet;
+
+	public Statistics(ArrayList<Tweet> tweet) {
+		super();
+		this.tweet = tweet;
+	}
+	
+}
