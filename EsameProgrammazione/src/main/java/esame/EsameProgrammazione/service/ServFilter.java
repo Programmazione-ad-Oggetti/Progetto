bdfb67b4373509b6d8fc46a2813b6ef8f1ae06a1
CarmenAndreozzi <64028178+CarmenAndreozzi@users.noreply.github.com>
2020-06-03 18:33:07 +0200

    Modifica della classe ServFilter.java

--- EsameProgrammazione/src/main/java/esame/EsameProgrammazione/service/ServFilter.java
+++ EsameProgrammazione/src/main/java/esame/EsameProgrammazione/service/ServFilter.java
@@ -1,6 +1,12 @@
 package esame.EsameProgrammazione.service;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+
+import esame.EsameProgrammazione.exceptions.FilterIllegalArgumentException;
+import esame.EsameProgrammazione.exceptions.FilterNotFoundException;
+import esame.EsameProgrammazione.exceptions.InternalGeneralException;
 
 import esame.EsameProgrammazione.model.Tweet;
 import esame.EsameProgrammazione.filter.Filter;
@@ -14,8 +20,57 @@ import esame.EsameProgrammazione.filter.Filter;
  * 	@author Carmen Andreozzi
  */
 
-
 public class ServFilter {
+	
+	private ArrayList<Tweet> EseguiFiltro;
+	private final static String path = "esame.EsameProgrammazione.filter.";
+	
+	public static Filter instanceFilter(String column,String operator,Object param) 
+			   throws FilterNotFoundException, FilterIllegalArgumentException,InternalGeneralException{
+			
+			Filter filtro;
+			String filterName = new String("Filter"+column+operator);
+			String ClassFilterName = path.concat(filterName);
+		    
+			try {
+				
+				Class<?> cls = Class.forName(ClassFilterName); //seleziono la classe
+			
+				Constructor<?> ct = cls.getDeclaredConstructor(Object.class); //seleziono il costruttore
+		    
+				filtro =(Filter)ct.newInstance(param);  //istanzo oggetto filtro
+			}
+			
+		    //entra qui se il nome filtro non è corretto 
+		    catch(ClassNotFoundException e){
+		    	throw new FilterNotFoundException("The filter in field: '"+column+"' with operator: '"+
+		                                          operator +"' does not exist");
+		    }
+			
+			//entra qui se sbagliate maiuscole e minuscole
+		    catch(NoClassDefFoundError e){
+		    	throw new FilterNotFoundException(
+		    			"Error typing: '"+filterName+"' uppercase and lowercase error");
+		    }
+
+		    //entra qui se il costruttore chiamato da newInstance lancia un eccezione 
+		   	catch (InvocationTargetException e) {  
+		   		//genero una nuova eccezione 
+		   		throw new FilterIllegalArgumentException(e.getTargetException().getMessage()
+		   				+ " Expected in '"+column+"'");
+		   	}
+
+		    catch(LinkageError | NoSuchMethodException | SecurityException 
+			    	   | InstantiationException | IllegalAccessException e ) {
+			    	
+			    	e.printStackTrace();
+			    	throw new InternalGeneralException("try later");
+			    }
+
+			
+		    return filtro;
+		    
+		}
 	
 	/**
 	 * Questo metodo restitusce una lista di tweet composta da soli tweet 
