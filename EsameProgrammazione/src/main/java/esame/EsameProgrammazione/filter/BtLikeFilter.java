0f17728abbd2499c84159d7813f0d106903f1168
AntoColu <antonioby99@hotmail.it>
2020-06-04 18:26:13 +0200

    Creazione della classe BtLikeFilter.java

--- EsameProgrammazione/src/main/java/esame/EsameProgrammazione/filter/BtLikeFilter.java
+++ EsameProgrammazione/src/main/java/esame/EsameProgrammazione/filter/BtLikeFilter.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 
 import esame.EsameProgrammazione.model.Tweet;
 
-public class GtLikeFilter implements Filter{
-	
+public class BtLikeFilter implements Filter{
 	private String field = "favorite_count";
-	private String operator = "$gt";
+	private String operator = "$bt";
 	
 	public String getField() {
 		return field;
@@ -20,10 +19,22 @@ public class GtLikeFilter implements Filter{
 	
 	@Override
 	public boolean filter(ArrayList<String> value, ArrayList<Tweet> tweet) {
+		double num1 = Double.parseDouble(value.get(0));
+		double num2 = Double.parseDouble(value.get(1));
+		double min;
+		double max; 
+		
+		if (Math.max(num1, num2) == num1) {
+			max = num1;
+			min = num2;
+		} else {
+			max = num2;
+			min = num1;
+		}
+		
 		for (int i = 0; i < tweet.size(); i++)
-			if (!(tweet.get(i).getLike() > Double.parseDouble(value.get(0))))
+			if (!(min < tweet.get(i).getLike() && max > tweet.get(i).getLike()))
 				return false;
 		return true;
 	}
-	
 }
