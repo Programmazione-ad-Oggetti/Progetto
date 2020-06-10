c77ae56ec942a3dbec82ae6910f7b004c217a562
AntoColu <antonioby99@hotmail.it>
2020-06-10 17:08:44 +0200

    Modifica classe Instruction.java

--- EsameProgrammazione/src/main/java/esame/EsameProgrammazione/model/Instruction.java
+++ EsameProgrammazione/src/main/java/esame/EsameProgrammazione/model/Instruction.java
@@ -3,8 +3,6 @@ package esame.EsameProgrammazione.model;
 import java.util.ArrayList;
 
 public class Instruction {
-<<<<<<< HEAD
-	
 	
 	//INTRODUZIONE DELL'ISTRUZIONE
 	private String introduzione = "";
@@ -44,47 +42,5 @@ public class Instruction {
 		for(int i=0;i<choice.length;i++)
 			this.scelta.add(choice[i]);
 	}
-=======
-	private String introduzione = "";
 
-	/**
-	 * An array with the different possibilities.
-	 */
-	private ArrayList<String> scelta = new ArrayList<String>();
-
-	/**
-	 * Getter for variable introduction
-	 * @return introduction
-	 */
-	public String getIntroduzione() {
-		return introduzione;
-	}
-
-	/**
-	 * Setter for variable introduction
-	 * @param intro the intro to set
-	 */
-	 public void setIntroduzione(String intro) {
-		this.introduzione = intro;
-	}
-
-	/**
-	 * Getter for the ArrayList possibilities
-	 * @return possibilities
-	 */
-	public ArrayList<String> getScelta() {
-		return scelta;
-	}
-
-	/**
-	 * Setter for the ArrayList possibilities
-	 * @param possibility to add at the end of the array
-	 */
-	 public void setPossibility(String[] choice) {
-		for(int i=0;i<choice.length;i++)
-			this.scelta.add(choice[i]);
-	}
-	
-	
->>>>>>> branch 'master' of https://github.com/Programmazione-ad-Oggetti/Progetto.git
 }
