f996c180c1ee48df8c60247cbb77118285a49757
CarmenAndreozzi <64028178+CarmenAndreozzi@users.noreply.github.com>
2020-06-11 19:09:55 +0200

    Creazione del file testo Instruction.txt

--- EsameProgrammazione/src/main/java/esame/EsameProgrammazione/controller/ControllerClass.java
+++ EsameProgrammazione/src/main/java/esame/EsameProgrammazione/controller/ControllerClass.java
@@ -5,6 +5,7 @@ import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import esame.EsameProgrammazione.model.Hashtag;
+import esame.EsameProgrammazione.service.InstructionReader;
 import esame.EsameProgrammazione.service.ServTweetsImpl;
 
 @RestController
 public class ControllerClass {
 	@Autowired
 	ServTweetsImpl ServTweetsImpl;
+	
+	
+	@GetMapping("/GetInstructions")
+	public ResponseEntity<Object> getInstruction(){
+		return new ResponseEntity<>(InstructionReader.getInstructions(),HttpStatus.OK);
+	}
 	
 	//VIENE RESTITUITA LA LISTA DEI PRODOTTI
 	@RequestMapping(value = "/tweets/{hashtag}", method = RequestMethod.GET)
--- EsameProgrammazione/src/main/java/esame/EsameProgrammazione/service/InstructionReader.java
+++ EsameProgrammazione/src/main/java/esame/EsameProgrammazione/service/InstructionReader.java
@@ -1,5 +1,42 @@
 package esame.EsameProgrammazione.service;
 
-public class InstructionReader {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import esame.EsameProgrammazione.model.Instruction;
+
+public class InstructionReader extends Instruction {
 	
+	private static ArrayList<Instruction> listaIstruzioni = new ArrayList<Instruction>();
+
+	public InstructionReader() {
+		Instruction istruzione;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("Instruction.txt"));
+			
+			String[] istruzioni = reader.readLine().split("&");
+			for(int i = 0; i < istruzioni.length; i++) {
+				
+				istruzione = new Instruction(); 
+				istruzione.setIntroduzione(istruzioni[i]);
+				i++;
+				
+				String[] array = istruzioni[i].split(";");
+				istruzione.setScelta(array);
+				listaIstruzioni.add(istruzione);
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException s) {
+			s.printStackTrace();
+		}
+	}
+
+	public static ArrayList<Instruction> getInstructions() {
+		return listaIstruzioni;
+	}
 }
