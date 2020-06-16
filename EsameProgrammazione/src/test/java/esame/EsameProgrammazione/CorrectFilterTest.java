3402f2815f3082dc94d6be82ccf48d14bd439e26
AntoColu <antonioby99@hotmail.it>
2020-06-16 12:07:38 +0200

    Modifica della classe CorrectFilterTest.java

--- EsameProgrammazione/src/test/java/esame/EsameProgrammazione/CorrectFilterTest.java
+++ EsameProgrammazione/src/test/java/esame/EsameProgrammazione/CorrectFilterTest.java
@@ -9,6 +9,12 @@ import org.junit.jupiter.api.Test;
 import esame.EsameProgrammazione.exceptions.*;
 import esame.EsameProgrammazione.service.*;
 
+/**
+ * Classe usata per testare l'eccezione in caso di filtro non corretto
+ * 
+ * @author Colucci Antonio
+ * @author Andreozzi Carmen
+ */
 class CorrectFilterTest {
 
 	@BeforeEach
@@ -21,12 +27,15 @@ class CorrectFilterTest {
 
 	@Test
 	void test() {
+		//Se uno dei tre parametri non è corretto allora viene lanciata l'eccezione FilterNotFoundException
 		assertThrows(FilterNotFoundException.class, ()-> ServFilter.instanceFilter("likes", "Gt" , 100));		
 		assertThrows(FilterNotFoundException.class, ()-> ServFilter.instanceFilter("flollower", "Lt", 250));
 		assertThrows(FilterNotFoundException.class, ()-> ServFilter.instanceFilter("Like", "Gtd", 200));
 		assertThrows(FilterNotFoundException.class, ()-> ServFilter.instanceFilter("Followers", "Ltd", 300));
-		assertThrows(FilterIllegalArgumentException.class, ()-> ServFilter.instanceFilter("Friends", "Gt", "ciao"));
 		assertThrows(FilterNotFoundException.class, ()-> ServFilter.instanceFilter("Friends", "ciao", 200));
+		
+		//Se il terzo parametro non è corretto allore viene lanciata l'eccezione FilterIllegalArgumentException
+		assertThrows(FilterIllegalArgumentException.class, ()-> ServFilter.instanceFilter("Friends", "Gt", "ciao"));
 	}
 
 }
